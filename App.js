ac002e4dd10a9f57498d60d0a61a25df605c4e4a
Ganesh Padhayay <ganeshraj020794@gmail.com>
2020-08-26 12:58:30 +0530

    code push can be tested on release build only, it is working fine

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <>
       <View style={styles.body}>
-        <Text>Hello</Text>
+        <Text>Hello World</Text>
       </View>
       <BookComponent />
     </>
